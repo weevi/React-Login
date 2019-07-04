04cd8de1e643913a83ad679e4acb39437f4e3e4e
weevi <paskeviciute.rima@gmail.com>
2019-07-04 12:53:06 +0000

    add form

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Form, Button } from 'semantic-ui-react'
+import { Form, Button, FormField } from 'semantic-ui-react'
 
 class LoginForm extends React.Component {
 state = {
@@ -7,9 +7,37 @@ state = {
     loading: false,
     error: {}
 };
+onChange = e => {
+    this.setState({
+        data: { ...this.state.data, [e.target.name]: [e.target.value]}
+    })
+}
+
     render(){
         return (
-            <Form><Button primary>Login</Button></Form>
+            <Form>
+                <Form.Field>
+                    <label htmlFor="email">Email</label>
+                    <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="example@example.com"
+                    value={this.onChange}
+                    />
+                </Form.Field>
+                <Form.Field>
+                    <label htmlFor="password">Password</label>
+                    <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="Make it secure"
+                    value={this.onChange}
+                    />
+                </Form.Field>
+                <button class="ui button">Login</button>
+                </Form>
         )
     }
 }
