517c8732c79df5011360e453b2584e027705ca5c
weevi <paskeviciute.rima@gmail.com>
2019-07-05 14:51:03 +0000

    add export default line in LoginForm component

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Form, Button, FormField } from 'semantic-ui-react'
+import { Form } from 'semantic-ui-react'
+
 
 class LoginForm extends React.Component {
 state = {
